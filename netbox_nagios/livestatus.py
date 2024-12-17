c06f1ec4ca83edd05ddbb6015cf3bc6b820a2545
Brynjar Friis Aune <brynjar.aune@dualog.com>
2024-12-17 10:54:07 +0100

    corrected url's, and added services to the nagios object

--- netbox_nagios/livestatus.py
+++ netbox_nagios/livestatus.py
@@ -14,6 +14,7 @@ def hoststatus(hostname: str, api_url: str, api_key: str):
         data = response.json()
         for host in data.get("hoststatus", []):
             if host.get("host_name") == hostname:
+                host["services"] = servicestatus(hostname, api_url, api_key)
                 return host
     except requests.exceptions.RequestException as e:
         print(f"Error fetching host status: {e}")
@@ -22,12 +23,20 @@ def hoststatus(hostname: str, api_url: str, api_key: str):
     return None
 
 
+def servicestatus(hostname: str, api_url: str, api_key: str):
+    url = f"{api_url}/objects/servicestatus?apikey={api_key}"
 
+    try:
+        response = requests.get(url, timeout=TIMEOUT)
+        response.raise_for_status()
 
-
-
-
-
-
-
-
+        services = []
+        data = response.json()
+        for service in data.get("servicestatus", []):
+            if service.get("host_name") == hostname:
+                services.append(service)
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching service status: {e}")
+        return None
+    
+    return services
